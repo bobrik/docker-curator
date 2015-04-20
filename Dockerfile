7c3e1dc8cd4d23e07c73eaa958d3e392ba2d44c1
Martin Meyer <martin.meyer@inftec.ch>
2015-04-20 10:44:43 +0200

    Improved script and Dockerfile
    - Based on PR input from bobrik

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM alpine:3.1
 RUN apk --update add python py-pip && \
     pip install elasticsearch-curator==3.0.3
 
-ADD curator-entrypoint.sh / 
+ADD ./curator-entrypoint.sh /curator-entrypoint.sh
 
 ENTRYPOINT ["/curator-entrypoint.sh"]
--- curator-entrypoint.sh
+++ curator-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+set -e
+
 # Automatically set host and port parameters if elasticsearch container is linked with this curator container
-if [[ "$ELASTICSEARCH_PORT_9200_TCP_ADDR" != "" ]]; then
+if [ ! -z "$ELASTICSEARCH_PORT_9200_TCP_ADDR" ]; then
   set -- --host $ELASTICSEARCH_PORT_9200_TCP_ADDR --port $ELASTICSEARCH_PORT_9200_TCP_PORT $@
 fi
 
-/usr/bin/curator $@
+exec /usr/bin/curator $@
